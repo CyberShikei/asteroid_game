116466497b5e76b9fc252e3904fa11f64a6c436c
CyberShikei <jacopferreira@gmail.com>
2025-01-13 09:24:25 +0200

    Add main

---
+++ main.py
@@ -0,0 +1,16 @@
+#!venv/bin/python3
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+WELCOME_MESSAGE = "Welcome to Asteroids!"
+START_MESSAGE = "Starting asteroids!"
+
+
+def main():
+    print(START_MESSAGE)
+
+
+if __name__ == "__main__":
+    main()
